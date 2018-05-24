5f77b2b335b1c03dbb5f19ff70c6339a2a3ffc77
Edmund Situmorang <edmundsitumorang@gmail.com>
2018-05-24 10:59:23 +0700

    using fs.createWriteStream to write scraped to file

--- ex1.js
+++ ex1.js
@@ -7,14 +7,26 @@ var app = express();
 var port = 8000;
 
 
+    // var url = "http://google.com";
+    // request(url, function(err, resp, body) {
+    //     if(err) { 
+    //         console.log(err);
+    //     } else {
+    //         console.log(body);
+    //     }
+    // })
+
+var destination = fs.createWriteStream('./downloads/google.html');
 var url = "http://google.com";
-request(url, function(err, resp, body) {
-    if(err) { 
+request(url)
+    .pipe(destination);
+    .on('finish', function() {
+        console.log('done');
+    })
+    on('error', function(err) {
         console.log(err);
-    } else {
-        console.log(body);
-    }
-})
+    })
+
 app.listen(port);
 console.log('server is listening on ' + port);
 
