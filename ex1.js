276eb4895dab924dbe8fda55e20e82816d820fa9
Edmund Situmorang <edmundsitumorang@gmail.com>
2018-05-24 10:25:14 +0700

    install dependencies and test scraping on google

---
+++ ex1.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var app = express();
+var port = 8000;
+
+
+var url = "http://google.com";
+request(url, function(err, resp, body) {
+    if(err) { 
+        console.log(err);
+    } else {
+        console.log(body);
+    }
+})
+app.listen(port);
+console.log('server is listening on ' + port);
+
